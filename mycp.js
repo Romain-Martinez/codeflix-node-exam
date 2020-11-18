b23968bf648490af4520bb13ca37859100e661cd
Romain.Martinez <romainmartinez250@gmail.com>
2020-11-18 15:48:35 +0100

    .\mycp.js

---
+++ mycp.js
@@ -0,0 +1,11 @@
+const fs = require ("fs");
+const path = require ("path");
+
+const copyFile = () => {
+    if (process.argv[2] === "-r") {} else {
+        const read = fs.createReadStream(process.argv[2]);
+        const write = fs.createWriteStream(process.argv[3]);
+        read.pipe(write);
+    }
+}
+copyFile()
